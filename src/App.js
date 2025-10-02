e73a4cf6b417aa0f8c5bf9b5a6c00a7ab73bdcbc
mia780 <varadymia@gmail.com>
2025-10-02 14:27:09 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Pályaorientáció
+        <h2>
+          Ha pályaváltás, vagyis új, az eddigitől eltérő foglalkozás választása előtt áll, érdemes
+          tisztáznia, hogy milyen képességekkel rendelkezik. A képességek erősen befolyásolják, hogy
+          pályáján, foglalkozásának gyakorlásánál mennyire sikeres. Az alábbi kérdőív ebben jelenthet
+          segítséget: kitöltésével tájékozódhat a képességeiről, melyek jelentik az erősségeit és melyek
+          azok, amelyekben gyengébb.
+        </h2>
+        
+      </h1>
+    </div>
+  );
+}
+
+export default App;
